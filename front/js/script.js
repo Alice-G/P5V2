2d45c782e160082b71b57fd02c09d07d59a4cb00
Alice-G <so.gayat@gmail.com>
2022-06-08 12:21:05 +0200

    step 4 - display glitch ????

--- front/js/script.js
+++ front/js/script.js
@@ -11,8 +11,8 @@ async function getCatalog() {
   let response = await fetch(apiURL); // stars a GET request (default)
   // Storing data in form of JSON
   const catalog = await response.json();
-  console.log(catalog); //DEL
-  console.log(typeof catalog); //DEL
+  // console.log(catalog); //DEL
+  // console.log(typeof catalog); //DEL
   if (response) {
     displayCatalog(catalog);
   }
@@ -22,14 +22,11 @@ getCatalog();
 
 // function to define innerHTML
 function displayCatalog(catalog) {
-  // let tab = '<div class="boop" style="border: 1px dashed purple">'; //container of things
   //Loop to access all rows
-
-  // HINT
 
   for (let item of catalog) {
     let newDiv = document.createElement('div');
-    let newContent = `<a href="./product.html?id=${item._id}">
+    let newContent = `<a href="./product.html?id=${item._id} data-id="${item._id}">
       <article>
         <img
           src="${item.imageUrl}"
@@ -40,7 +37,6 @@ function displayCatalog(catalog) {
       </article>
     </a>`;
 
-    // HINT
     newDiv.innerHTML = newContent;
 
     //setting innerHTML as  tab
@@ -48,13 +44,86 @@ function displayCatalog(catalog) {
   }
 }
 
+/// A PARTIR D'ICI C'EST LA ZONE SUR LAQUELLE JE SUIS EN TRAIN DE BOSSER C'EST LE BAZAR C'EST NORMAL
+
 // TODO save url search param onclick?
+
+let chosenProduct;
+let cards = document.getElementById('items').children;
+console.log('this is cards:', cards); //DEL
+
+// function getProductUrl(e) {
+//   var chosenID = e.getAttribute('data-id');
+//   console.log(chosenID);
+//   chosenProduct = chosenID;
+// }
+
+// HINT I checked whether it exists first with if (cards !== null)  // not it
+for (meh of cards) {
+  console.log(meh);
+  meh.addEventListener('hover', () => {
+    console.log(meh);
+  });
+}
+
+// cards.addEventListener('click', getProductUrl);
+
+// do add event listeniner here
+
+// function storeUrl() {
+//   var str =
+//     'https://waytolearnx.com/t.html?name=alex-babtise&age=25&address=paris';
+
+//   var url = new URL(str);
+
+//   var search_params = new URLSearchParams(url.search);
+
+//   if (search_params.has('_id')) {
+//     var name = search_params.get('_id');
+//     console.log(_id);
+//     chosenProduct = _id;
+//     console.log(chosenProduct);
+//   }
+// }
+// // should it be 'items' here? or newDiv?
+
+// document.getElementById('items').addEventListener(onclick, chosenProduct);
 
 // BLOCK            PRODUCT PAGE
 
+// TODO trying to extract url from product page directly because i followed it?
+
+// window.location.href = URL;
+// console.log(URL);
+// Then you can just grab the params from the url. For instance, if the url is https://example.com/products?id=142 then you can grab the id like this:
+
+// const urlParams = new URLSearchParams(URL);
+
+// const id = urlParams.get('id');
+
 // TODO use url search param to extract id
+
 // TODO if no id, default back to home page?
 // TODO store id
 // TODO take id into the fetch GET
+
 // TODO display stuff
-// TODO
+
+// MAKE IT A LOOP THROUGH WITH FETCH??
+
+// DISPLAY STUFF ABOUT PRODUCT
+// product pic
+//            document.querySelector('.item_img')
+//product name
+//          document.getElementById('title').innerHTML = `${item.name}`;
+// product price
+//            id = 'price';
+// product description
+//                id = 'description';
+
+// TODO how do do colors?
+
+/* <select name="color-select" id="colors">
+<option value="">--SVP, choisissez une couleur --</option> 
+<option value="vert">vert</option>
+                      <option value="blanc">blanc</option> */
